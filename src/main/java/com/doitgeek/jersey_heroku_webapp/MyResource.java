492df12cb5fff2f20b9f4b694397d5574ba620a1
Emanuele Palomba <emanuepalomba@gmail.com>
2021-09-10 16:41:59 +0200

    Metodo Create
    Creazione Metodo Create

--- src/main/java/com/doitgeek/jersey_heroku_webapp/MyResource.java
+++ src/main/java/com/doitgeek/jersey_heroku_webapp/MyResource.java
@@ -1,6 +1,8 @@
 package com.doitgeek.jersey_heroku_webapp;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -15,5 +17,13 @@ public class MyResource {
         return "Hello, Heroku!";
     }
     
+//    @POST
+//    @Path("/create/{note}")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Note createNote(Long id,String title,String author) {
+//    	Note note = new Note(id,title,author);
+//    	return note;
+//    }
     
 }
--- src/main/java/com/doitgeek/jersey_heroku_webapp/NoteDataService.java
+++ src/main/java/com/doitgeek/jersey_heroku_webapp/NoteDataService.java
@@ -1,5 +1,20 @@
 package com.doitgeek.jersey_heroku_webapp;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("note")
 public class NoteDataService {
 
+	@POST
+    @Path("/create/{note}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Note createNote(Long id,String title,String author) {
+    	Note note = new Note(id,title,author);
+    	return note;
+    }
 }
